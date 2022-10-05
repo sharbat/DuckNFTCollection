2d29b9fb9407cd226422470236f6dfb1ec35648e
Sharbat Rakhman <sharbat021515@gmail.com>
2022-10-05 12:03:40 +0600

    Token ID

--- pages/api/[tokenId].js
+++ pages/api/[tokenId].js
@@ -1,16 +1,16 @@
 export default function handler(req, res) {
     // get the tokenId from the query params
-    const tokenId = req.query.tokenId;
+    const tokenId = req.query.tokenId.match(/\d*/);
     // As all the images are uploaded on github, we can extract the images from github directly.
     const image_url =
-      "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
+      "https://raw.githubusercontent.com/sharbat/DuckNFTCollection/master/public/cryptodevs/";
     // The api is sending back metadata for a Crypto Dev
     // To make our collection compatible with Opensea, we need to follow some Metadata standards
     // when sending back the response from the api
     // More info can be found here: https://docs.opensea.io/docs/metadata-standards
     res.status(200).json({
-      name: "Crypto Dev #" + tokenId,
-      description: "Crypto Dev is a collection of developers in crypto",
-      image: image_url + tokenId + ".svg",
+      name: "Duck #" + tokenId,
+      description: "This NFT was generated in  Adobe Photoshop",
+      image: image_url + tokenId + ".png",
     });
   }
